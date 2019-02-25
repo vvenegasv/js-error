ebc0d6f7656d374adc2037786cd979fff0f6e9d9
Vladimir Venegas <vladimir@becual.com>
2019-02-25 19:40:34 -0300

    try to deploy transpiled source

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: slsw.lib.entries,
+  entry: './index.js',
   target: 'node',
   // Generate sourcemaps for proper error messages
   devtool: 'source-map',
   // Since 'aws-sdk' is not compatible with webpack,
   // we exclude all node dependencies
   externals: [nodeExternals()],
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  mode: 'production',
   optimization: {
     minimize: true,
   },
